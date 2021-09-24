2463ce1bb4dbdb1aa27b9786e063fa1786ad6d61
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 12:42:39 +0530

    Create Pattern 4.c

---
+++ Patterns/Pattern 4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+      printf("input = %d\n",n);
+    for(i=5;i>=1;i--)
+    {
+        for(j=1;j<=n;j++)
+        {
+        printf("%d ",i);
+        }
+    
+    printf("\n");
+}
+    return 0;
+}
+
+OUTPUT: 
+5
+
+input = 5
+
+5 5 5 5 5 
+
+4 4 4 4 4 
+
+3 3 3 3 3 
+
+2 2 2 2 2 
+
+1 1 1 1 1 
+
+
+
