950000d649364455c603a44f7da5de6dee968e0f
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 17:59:01 +0530

    Create Pattern 17.c

---
+++ Patterns/Pattern 17.c
@@ -0,0 +1,37 @@
+5
+
+ 0  1  0  1  0 
+
+ 1  0  1  0  1 
+
+ 0  1  0  1  0 
+
+ 1  0  1  0  1 
+
+ 0  1  0  1  0 
+
+
+
+
+
+#include <stdio.h>
+
+void main()
+{
+    int n,i,j,x;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {    
+            x=(i+j)%2;
+           
+                
+             printf("%2d ",x);
+             }
+           
+         printf("\n");
+       
+    }
+}     
+  
