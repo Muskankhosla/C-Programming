de0f77b2fbbf6fe20cfc48e3b558be0fd29715d2
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 18:55:25 +0530

    Create Pattern 28.c

---
+++ Patterns/Pattern 28.c
@@ -0,0 +1,24 @@
+
+E E E E E 
+D D D D D 
+C C C C C 
+B B B B B 
+A A A A A 
+
+#include <stdio.h>
+
+void main()
+{
+    char ch,i,j;
+    for(i='E';i>='A';i--)
+    {
+        for(j='A';j<='E';j++)
+        {   
+             printf("%c ",i);
+            
+        } 
+         printf("\n");
+       
+    }
+}     
+
