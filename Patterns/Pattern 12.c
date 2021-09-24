b427fa37c77de0c5e6c6a4280de59ce27e58f43f
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 13:50:56 +0530

    Create Pattern 12.c

---
+++ Patterns/Pattern 12.c
@@ -0,0 +1,26 @@
+5
+  
+1  6 11 16 21 
+2  7 12 17 22 
+3  8 13 18 23 
+4  9 14 19 24 
+5 10 15 20 25 
+
+
+
+#include <stdio.h>
+
+void main()
+{
+    int i,j,n,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {   k=n*(j-1)+i;
+            printf("%2d ",k);
+        }
+        printf("\n");
+        
+    }
+}
