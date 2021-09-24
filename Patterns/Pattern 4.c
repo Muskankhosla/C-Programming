7b408ac1ec4a6b7c7d3e275df42fa8e15b90e7e5
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 16:35:40 +0530

    Update Pattern 4.c

--- Patterns/Pattern 4.c
+++ Patterns/Pattern 4.c
@@ -1,5 +1,16 @@
-#include <stdio.h>
 
+OUTPUT: 
+5
+input = 5
+5 5 5 5 5 
+4 4 4 4 4 
+3 3 3 3 3 
+2 2 2 2 2 
+1 1 1 1 1 
+
+
+
+#include <stdio.h>
 int main()
 {
     int n,i,j;
@@ -16,15 +27,4 @@ int main()
 }
     return 0;
 }
-
-OUTPUT: 
-5
-input = 5
-5 5 5 5 5 
-4 4 4 4 4 
-3 3 3 3 3 
-2 2 2 2 2 
-1 1 1 1 1 
-
-
 
