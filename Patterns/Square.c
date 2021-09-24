2fab75b5c95ba9cb542e44485ab8a2c5229c3baf
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 12:26:55 +0530

    Create Square.c

---
+++ Patterns/Square.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    printf("input = %d\n",n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+        printf("*");
+        }
+    
+    printf("\n");
+}
+    return 0;
+}
+
+
+
+5
+*****
+*****
+*****
+*****
+*****
+
+
+
