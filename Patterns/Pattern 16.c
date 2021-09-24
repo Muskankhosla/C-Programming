179005a78f8f44c13b8bcdc1b1e41721f5340b7c
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 16:42:42 +0530

    Update Pattern 16.c

--- Patterns/Pattern 16.c
+++ Patterns/Pattern 16.c
@@ -1,28 +1,28 @@
 5
 
-1 2 3 4 5 6 
+ 1  2  3  4  5 
 
-2 3 4 5 6 7 
+ 2  3  4  5  6 
 
-3 4 5 6 7 8 
+ 3  4  5  6  7 
 
-4 5 6 7 8 9 
+ 4  5  6  7  8 
 
-Hello World
+ 5  6  7  8  9 
 
 
 
 #include <stdio.h>
-
 int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n-1;i++)
+    for(i=1;i<=n;i++)
     {
-        for(j=i;j<=n+i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d ",j);
+            printf("%2d ",i+j-1);
+            
         }
         printf("\n");
     }
