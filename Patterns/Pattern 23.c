21e3c46d7a5b21e56575cd1aad8baa4ae3f36b3b
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-24 18:45:22 +0530

    Create Pattern 23.c

---
+++ Patterns/Pattern 23.c
@@ -0,0 +1,37 @@
+5
+
+1 1 1 1 1 
+
+0 0 0 0 0 
+
+1 1 1 1 1 
+
+0 0 0 0 0 
+
+1 1 1 1 1 
+
+
+
+
+
+#include <stdio.h>
+
+void main()
+{
+    int n,i,j,x;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=0;j<n;j++)
+        {   
+            if(i%2!=0)
+            {
+             printf("1 ");
+            }
+            else
+            printf("0 ");
+        } 
+         printf("\n");
+       
+    }
+}     
