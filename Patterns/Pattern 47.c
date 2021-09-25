5b5728e5dbd20d1fc00b1cb9aa6f6a7e765ba233
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-25 16:53:03 +0530

    Create Pattern 47.c

---
+++ Patterns/Pattern 47.c
@@ -0,0 +1,36 @@
+5
+
+1 
+
+2 4 
+
+3 6 9 
+
+4 8 12 16 
+
+5 10 15 20 25 
+
+
+
+
+#include <stdio.h>
+
+int main()
+{ 
+    int n,k=1,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    { 
+      for(j=1;j<=i;j++)
+      { 
+        
+        printf("%d ",i*j);
+         
+      }
+         
+      printf("\n");
+     
+    }
+
+    return 0;
+}
