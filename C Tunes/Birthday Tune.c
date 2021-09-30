a2a6a8ce6f7225b1abc9639bd39613f94fa67e95
Muskan Khosla <khosla.muskan01@gmail.com>
2021-09-30 15:19:54 +0530

    Create Birthday Tune.c

---
+++ C Tunes/Birthday Tune.c
@@ -0,0 +1,141 @@
+#include <dos.h>
+
+void main()
+{
+
+
+
+
+int j=0,a=391,b=440,c=493,d=523,e=587,f=659,g=698,h=783;
+	sound(a);
+	delay(250);
+	sound(j);
+	delay(20);
+	sound(a);
+	delay(250);
+	sound(b);
+	delay(250);
+	sound(a);
+	delay(250);
+	sound(d);
+	delay(250);
+	sound(c);
+	delay(500);
+	sound(j);
+	delay(300);
+
+
+	sound(a);
+	delay(250);
+	sound(j);
+	delay(20);
+	sound(a);
+	delay(250);
+	sound(b);
+	delay(250);
+	sound(a);
+	delay(250);
+	sound(e);
+	delay(250);
+	sound(d);
+	delay(500);
+
+	sound(a);
+	delay(250);
+	sound(a);
+	delay(250);
+	sound(h);
+	delay(250);
+	sound(f);
+	delay(250);
+	sound(d);
+	delay(250);
+	sound(c);
+	delay(250);
+	sound(b);
+	delay(400);
+
+	sound(g);
+	delay(250);
+	sound(g);
+	delay(250);
+	sound(f);
+	delay(250);
+	sound(d);
+	delay(250);
+	sound(e);
+	delay(250);
+	sound(d);
+	delay(600);
+
+	sound(a);
+	delay(250);
+	sound(j);
+	delay(250);
+	sound(a);
+	delay(250);
+	sound(b);
+	delay(250);
+	sound(a);
+	delay(250);
+	sound(d);
+	delay(250);
+	sound(c);
+	delay(500);
+	sound(j);
+	delay(200);
+
+		   //2
+	sound(a);
+	delay(250);
+	sound(j);
+	delay(20);
+	sound(a);
+	delay(250);
+	sound(b);
+	delay(250);
+	sound(a);
+	delay(250);
+	sound(e);
+	delay(250);
+	sound(d);
+	delay(500);
+
+
+
+	sound(a);
+	delay(250);
+	sound(a);
+	delay(250);
+	sound(h);
+	delay(250);
+	sound(f);
+	delay(250);
+	sound(d);
+	delay(250);
+	sound(c);
+	delay(250);
+	sound(b);
+	delay(400);
+
+
+
+
+	sound(g);
+	delay(250);
+	sound(g);
+	delay(250);
+	sound(f);
+	delay(250);
+	sound(d);
+	delay(250);
+	sound(e);
+	delay(250);
+	sound(d);
+	delay(600);
+	nosound();
+	nosound();
+
+	getch();
+	return 0;
+}
