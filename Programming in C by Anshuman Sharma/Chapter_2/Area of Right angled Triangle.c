d39a4fd0b5a02e27e831d532485d1d309d2f823f
Muskan Khosla <khosla.muskan01@gmail.com>
2021-10-08 11:21:09 +0530

    Create Area of Right angled Triangle.c

---
+++ Programming in C by Anshuman Sharma/Chapter_2/Area of Right angled Triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,area;
+ 
+    printf("Enter the Numbers:");
+    scanf("%d %d",&a,&b);
+    area=(a*b)/2;
+    printf("area of Right angled triangle=%d",area);
+}
