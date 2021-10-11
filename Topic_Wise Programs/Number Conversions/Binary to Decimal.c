d39b7354e41bd0bbdc117d8477cbc4957b7b2b99
Muskan Khosla <khosla.muskan01@gmail.com>
2021-10-11 16:30:56 +0530

    Update Binary to Decimal.c

--- Topic_Wise Programs/Number Conversions/Binary to Decimal.c
+++ Topic_Wise Programs/Number Conversions/Binary to Decimal.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{ 
+    int bin,i=0,dec=0,rem;
+  scanf("%d",&bin);
+  while (bin!= 0)  
+    {  
+        rem = bin% 10;  
+        bin= bin / 10;  
+        dec = dec + rem * pow( 2,i);  
+        i++;  
+    }  
+    printf("%d",dec);
+
+    return 0;
+}
 
